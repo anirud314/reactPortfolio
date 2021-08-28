f5cd905c94744554e967131fa552c1cceda603f6
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-08-28 12:29:47 -0700

    Fixed file structure, Structured app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -20,6 +20,24 @@ function App() {
       </header>
     </div>
   );
+}*/
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Contact />
+
+    </main>
+  )
 }
 
 export default App;
